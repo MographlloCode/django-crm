c47e4bf2cbbccc0ab87a02f28b831148056a7f5d
Gustavo Mello <mographllo@gmail.com>
2023-09-09 23:10:56 -0300

    login system added

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     ''' Home Page View ''' 
+    
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was an error logging in, please, try again...')
+            return redirect('home')
+    
     return render(request, 'home.html', {})
+
+def logout_user(request):
+    ''' Logout User '''
+    pass
+
